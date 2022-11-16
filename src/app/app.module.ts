173edeae88081f123f12480c348ed72419f11f0b
juliofernandez98 <juliofernandez@lksnext.com>
2022-11-16 16:42:11 +0100

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp1Component
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
